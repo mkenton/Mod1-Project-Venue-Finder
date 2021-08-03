568d0924c22a2942bbae780257e9d0377d47621b
Sungoh Park <spark449062@gmail.com>
2021-08-03 11:46:32 -0600

    added address input with vote and delete button

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Initialize and add the map
+const addressInputForm = document.querySelector('form#addressInputForm')
+const addressSubmitButtonElement = document.querySelector('button#submitAddress');
+const addressInputTextElement = document.querySelector('input#addressTextInput');
+const addressDisplayParentElement = document.querySelector('div#addressList');
 function initMap() {
     const newYorkCoord = { lat: 40.7128, lng: -74.0060 };
   
@@ -15,4 +19,37 @@ function initMap() {
     });
   }
   
+    addressSubmitButtonElement.addEventListener('click', function(event) {
+      event.preventDefault();
+      const newAddress = document.createElement("p");
+      newAddress.innerText = addressInputTextElement.value + " ";
+
+      const newVoteButton = document.createElement("button");
+      newVoteButton.setAttribute('class', 'vote');
+      newVoteButton.innerText = "vote";
+
+      const newDeleteButton = document.createElement("button");
+      newDeleteButton.setAttribute('class', 'delete');
+      newDeleteButton.innerText = 'x';
+      newDeleteButton.addEventListener('click', function(event) {
+        event.target.parentNode.remove();
+      })
+
+      addressDisplayParentElement.append(newAddress);
+      newAddress.append(newVoteButton);
+      newAddress.append(newDeleteButton)
+      addressInputForm.reset();
+    })
+
+
+  //   let deleteButton = document.createElement('button');
+  //   deleteButton.textContent = 'x';
+  //   deleteButton.addEventListener('click', handleDelete)
   
+  //   taskItem.appendChild(deleteButton);
+  //   document.querySelector("#tasks").appendChild(taskItem);
+  // }
+  
+  // function handleDelete(event) {
+  //   event.target.parentNode.remove();
+  // }
