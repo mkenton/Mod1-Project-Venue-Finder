9d17ff293eb9c7b9194397ac2cdd67ae52dce7bb
Sungoh Park <spark449062@gmail.com>
2021-08-03 11:01:14 -0600

    inserted google map

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-<script type="text/javascript" src="./config.js"></script> 
+// Initialize and add the map
+function initMap() {
+    const newYorkCoord = { lat: 40.7128, lng: -74.0060 };
+  
+    const originCoord = newYorkCoord;
+    // The map, centered at (0,0)
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: originCoord,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: originCoord,
+      map: map,
+    });
+  }
+  
+  
